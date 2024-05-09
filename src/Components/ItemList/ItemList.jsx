e4f193bb8bc815a04a28616e0be8cb5f8f39d682
ElyCordova <elycordovamiranda@gmail.com>
2024-05-08 20:47:18 -0600

    general changes

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -3,8 +3,9 @@ import './ItemList.css';
 
 const ItemList = ({ items }) => {
 return (
+    
    <div className="item-list">
-    <h2>Nuestros productos:</h2>
+    <h2>Nuestros productos:</h2> 
     {items.map((item) => (
         <Item key={item.id} item={item}/>
     ))}
