db8d1c11c8fa81b9f5c6f8da447d711426a06708
Javier Veron <javier.veron@gmail.com>
2024-05-16 21:07:19 -0300

    Firebase 1

--- src/Components/ItemList/Item.jsx
+++ src/Components/ItemList/Item.jsx
@@ -5,10 +5,10 @@ const Item = ({ item }) => {
         <div className="col-md-4 mb-4">
             <Link to={"/item/" + item.id}>
                 <div className="card">
-                <img src={item.imageUrl} alt={item.name} className="img-fluid" />
+                <img src={item.imagen} alt={item.nombre} className="img-fluid" />
                     <div className="card-body">
-                        <h5 className="card-title text-secondary">{item.name}</h5>
-                        <p className="item-price text-secondary"><b>${item.price}</b></p>
+                        <h5 className="card-title text-secondary">{item.nombre}</h5>
+                        <p className="item-price text-secondary"><b>${item.precio}</b></p>
                     </div>
                 </div>
             </Link>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './Components/NavBar/NavBar.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdZoZbG-axKp_wCqR4d9YMsZ2XM1eXhxI",
+  authDomain: "coderhouse-61565.firebaseapp.com",
+  projectId: "coderhouse-61565",
+  storageBucket: "coderhouse-61565.appspot.com",
+  messagingSenderId: "100541130228",
+  appId: "1:100541130228:web:eeda762557af43dc3ac5a1"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
